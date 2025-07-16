aa6ee0d9a40c7769c0e313afb367324d5ec9e75c
aveeeeen <taichimatsumoto360@gmail.com>
2025-07-16 13:03:18 +0900

    added duration change and stop button, and better fadein fadeout implementation

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,12 @@ import { ref, onMounted } from 'vue';
 import SelectorButton from './components/SelectorButton.vue';
 import SelectorButtonEditor from './components/SelectorButtonEditor.vue';
 import MiniMap from './components/MiniMap.vue';
+import StopButton from './components/StopButton.vue';
 
 type Effect = {
   id: number;
   name: string;
 };
-const webSocketUrl = 'ws://localhost:8080';
-let webSocket: WebSocket | null = null;
 
 const effectLength = 32;
 const effectList = ref<any>(Array.from({ length: effectLength }, (_, i) => ({
@@ -24,150 +23,152 @@ const encoderStatus = ref('standby')
 const selectedEffect = ref<null | Effect>(null);
 const isEditing = ref<boolean>(false);
 const gain = ref<number>(0.1);
+const signalDuration = ref<number>(0.5);
 
 // 音声送信
 
 const CONFIG = {
-            BASE_FREQ: 17000,      // 基本周波数
-            FREQ_RANGE: 2000,      // 周波数範囲
-            NUM_CHANNELS: 32,      // チャンネル数
-            SIGNAL_DURATION: 1.0,  // 信号長（秒）
-            SAMPLE_RATE: 44100,    // サンプリングレート
-            FFT_SIZE: 8192,        // FFT サイズ
-            DETECTION_THRESHOLD: 0.3 // 検出閾値
-        };
-        
-        // グローバル変数
-let audioContext : any= null;
-let hostOscillator : any= null;
-let hostGain : any = null;
+  BASE_FREQ: 17000,      // 基本周波数
+  FREQ_RANGE: 2000,      // 周波数範囲
+  NUM_CHANNELS: 32,      // チャンネル数
+  // SIGNAL_DURATION: 1.0,  // 信号長（秒）
+  SAMPLE_RATE: 44100,    // サンプリングレート
+  FFT_SIZE: 8192,        // FFT サイズ
+  DETECTION_THRESHOLD: 0.3 // 検出閾値
+};
+
+// グローバル変数
+let audioContext: any = null;
+let hostOscillator: any = null;
+let hostGain: any = null;
 let isHostActive = false;
-let signalFn : any = null;
+let interval: any = null;
 
 function getFrequencyForChannel(channel: number) {
-            const step = CONFIG.FREQ_RANGE / CONFIG.NUM_CHANNELS;
-            return CONFIG.BASE_FREQ + (channel * step);
-        }
+  const step = CONFIG.FREQ_RANGE / CONFIG.NUM_CHANNELS;
+  return CONFIG.BASE_FREQ + (channel * step);
+}
 
- async function initializeHost() {
-            try {
-                audioContext = new (window.AudioContext)();
-                
-                // ゲインノードを作成
-                hostGain = audioContext.createGain();
-                hostGain.connect(audioContext.destination);
-                hostGain.gain.setValueAtTime(0, audioContext.currentTime);
-                
-                isHostActive = true;
-                encoderStatus.value = "ホスト初期化に成功しました"
-                
-            } catch (error) {
-                console.error('ホスト初期化エラー:', error);
-                encoderStatus.value = 'ホスト初期化に失敗しました';
-            }
-        }
- 
+async function initializeHost() {
+  try {
+    audioContext = new (window.AudioContext)();
+
+    // ゲインノードを作成
+    hostGain = audioContext.createGain();
+    hostGain.connect(audioContext.destination);
+    hostGain.gain.setValueAtTime(0, audioContext.currentTime);
+
+    isHostActive = true;
+    encoderStatus.value = "ホスト初期化に成功しました"
+
+  } catch (error) {
+    console.error('ホスト初期化エラー:', error);
+    encoderStatus.value = 'ホスト初期化に失敗しました';
+  }
+}
+
 function delay(ms: number): Promise<boolean> {
-  return new Promise( (resolve) =>  {
+  return new Promise((resolve) => {
     setTimeout(() => {
       resolve(true);
     }, ms)
   })
-}       
+}
 
-        // エフェクト送信
- async function sendEffect(effectId: number) {
-            if (!isHostActive || !audioContext) return;
-            
-            const frequency = getFrequencyForChannel(effectId);
-            
-            let now = audioContext.currentTime;
-            hostGain.gain.linearRampToValueAtTime(0, now + 0.5);
+// エフェクト送信
+async function sendEffect(effectId: number, duration: number) {
+  if (!isHostActive || !audioContext) return;
 
-            // 既存のオシレーターを停止
-            
-            let wait = await delay(500);
-            
-            if(wait) {
+  const frequency = getFrequencyForChannel(effectId);
 
-              if (hostOscillator) {
-                hostOscillator.stop();
-              }
+  let now = audioContext.currentTime;
 
-              let retfn = () => {
-                
-                hostOscillator = audioContext.createOscillator();
-                hostOscillator.type = 'sine';
-                hostOscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
-                hostOscillator.connect(hostGain);
-                // エンベロープを設定（フェードイン・フェードアウト）
-                now = audioContext.currentTime;
-                hostGain.gain.cancelScheduledValues(now);
-                hostGain.gain.setValueAtTime(0, now);
-                hostGain.gain.linearRampToValueAtTime(gain.value, now + 0.1);
-                // hostGain.gain.setValueAtTime(0.1, now + CONFIG.SIGNAL_DURATION - 0.1);
-                hostGain.gain.linearRampToValueAtTime(0, now + CONFIG.SIGNAL_DURATION);
-                
-                // 信号を送信
-                hostOscillator.start(now);
-                hostOscillator.stop(now + CONFIG.SIGNAL_DURATION);
-                
-                // UI更新
-                
-                console.log(`エフェクト送信: ID=${effectId}, 周波数=${frequency}Hz`);
-              }
-  
-              signalFn = retfn;
-            }
-            // 新しいオシレーターを作成
-        }
+  if (hostOscillator) {
+    hostGain.gain.cancelScheduledValues(now);
+    hostGain.gain.setValueAtTime(hostGain.gain.value, now);
+    hostGain.gain.linearRampToValueAtTime(0, now + 0.01);
+    hostOscillator.stop(now + 0.01);
+  } else {
+    hostGain.gain.cancelScheduledValues(now);
+    hostGain.gain.setValueAtTime(0, now);
+  }
+  // hostGain.gain.linearRampToValueAtTime(0, now + 0.1);
+
+  // 既存のオシレーターを停止
+
+  let wait = await delay(10);
+
+  if (wait) {
+    if (interval) clearInterval(interval);
+    let retfn = () => {
+
+      hostOscillator = audioContext.createOscillator();
+      hostOscillator.type = 'sine';
+      hostOscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+      hostOscillator.connect(hostGain);
+      // エンベロープを設定（フェードイン・フェードアウト）
+      now = audioContext.currentTime;
+      hostGain.gain.setValueAtTime(0, now);
+      hostGain.gain.linearRampToValueAtTime(gain.value, now + 0.01);
+      hostGain.gain.setValueAtTime(gain.value, now + duration - 0.01);
+      hostGain.gain.linearRampToValueAtTime(0, now + duration);
+
+      // 信号を送信
+      hostOscillator.start(now);
+      hostOscillator.stop(now + duration);
+
+      // UI更新
+
+      console.log(`エフェクト送信: ID=${effectId}, 周波数=${frequency}Hz`);
+    }
+
+    interval = setInterval(() => {
+      if (typeof retfn === 'function') retfn();
+    }, (duration * 1000) * 1.5);
+  }
+  // 新しいオシレーターを作成
+}
+
+function stopEffect() {
+  if (!isHostActive || !audioContext) return;
+
+  selectedEffect.value = null;
+
+  let now = audioContext.currentTime;
+
+  if (hostOscillator) {
+    hostGain.gain.cancelScheduledValues(now);
+    hostGain.gain.setValueAtTime(hostGain.gain.value, now);
+    hostGain.gain.linearRampToValueAtTime(0, now + 0.01);
+    hostOscillator.stop(now + 0.01);
+  } else {
+    hostGain.gain.cancelScheduledValues(now);
+    hostGain.gain.setValueAtTime(0, now);
+  }
+
+  clearInterval(interval);
+}
 
 onMounted(() => {
   initializeHost();
-  connectWebSocket(); 
-  if(localStorage.getItem('effectList')) {
+  if (localStorage.getItem('effectList')) {
     effectList.value = JSON.parse(localStorage.getItem('effectList') || "[]");
-  } 
+  }
   else {
     localStorage.setItem('effectList', JSON.stringify(effectList.value));
   }
 })
 
-function connectWebSocket() {
-  if (webSocket && webSocket.readyState === WebSocket.OPEN) {
-    return; // Already connected
-  }
-  webSocket = new WebSocket(webSocketUrl);
-  webSocket.onopen = () => {
-    connectionStatus.value = 'Connected';
-  };
 
-  webSocket.onclose = () => {
-    connectionStatus.value = 'Disconnected';
-  };
-
-  webSocket.onerror = (error) => {
-    console.error('WebSocket Error:', error);
-    connectionStatus.value = 'Error';
-  };
-}
-
-setInterval(() => {
-  if(typeof signalFn === 'function') signalFn();
-}, CONFIG.SIGNAL_DURATION * 1000 * 2);
 
 function onSelectorButtonClick(effect: Effect) {
   selectedEffect.value = effect;
   if (audioContext) {
-    signalFn = sendEffect(effect.id);
-  }
-  if (!webSocket) return;
-  if (effect && webSocket.readyState === WebSocket.OPEN) {
-    webSocket.send(JSON.stringify({ action: 'selectEffect', effectId: effect.id }));
+    sendEffect(effect.id, signalDuration.value);
   }
 }
 
-function onSelectorEdit( props: Effect, index: number ){
+function onSelectorEdit(props: Effect, index: number) {
   effectList.value[index] = props;
   localStorage.setItem('effectList', JSON.stringify(effectList.value));
   console.log(`Effect ${index} updated:`, props);
@@ -192,48 +193,48 @@ function previousPage() {
     <div>
       <div>
         <h1>ASP Sync Effect Controller</h1>
-        <div class="websocket-status">
-          <p>Web Socket 接続状況: {{ connectionStatus }}</p>
-          <button @click="connectWebSocket">再接続</button>
+        <div class="flex-row gap-20">
+          <div>
+            <div class="flex-column">
+              <p>サウンドエンコーダーの状態： {{ encoderStatus }}</p>
+              <div class="flex-column">
+                <div class="flex-row">
+                  <input id="gain" type="range" step="0.1" min="0" max="1" v-model.number="gain">
+                  <label for="gain">gain</label>
+                  <p> {{ gain }} </p>
+                </div>
+                <div class="flex-row">
+                  <input id="duration" type="range" step="0.1" min="0.2" max="1" v-model.number="signalDuration">
+                  <label for="duration">duration</label>
+                  <p> {{ signalDuration }}</p>
+                </div>
+              </div>
+              <div class="flex-row">
+                <p>エフェクト名を編集する</p>
+                <button @click="isEditing = !isEditing">編集</button>
+              </div>
+              <p>選択中のエフェクト: {{ selectedEffect ? selectedEffect.name : 'なし' }}</p>
+            </div>
+          </div>
+          <div>
+            <StopButton :isSelected="!selectedEffect" @stop="stopEffect()"></StopButton>
+          </div>
         </div>
-        <div class="websocket-status">
-          <p>サウンドエンコーダーの状態： {{ encoderStatus }}</p>
-          <input id="gain" type="range" step="0.1" min="0" max="1" v-model="gain">
-          <label for="gain">Gain</label>
-          <p> {{ gain }} </p>
-        </div>
-        <div class="websocket-status">
-          <p>エフェクト名を編集する</p>
-          <button @click="isEditing = !isEditing">編集</button>
-        </div>
-        <p>選択中のエフェクト: {{ selectedEffect ? selectedEffect.name : 'なし' }}</p>
       </div>
       <div>
-        <MiniMap 
-          :effectList="effectList" 
-          :selectedEffect="selectedEffect"
-          :page="page"> 
+        <MiniMap :effectList="effectList" :selectedEffect="selectedEffect" :page="page">
         </MiniMap>
       </div>
     </div>
     <div>
       <div v-if="!isEditing" class="selector">
-        <SelectorButton 
-          v-for="effect in effectList.slice((page - 1) * 16, page * 16)" :key="effect.id" 
-          :id="effect.id" 
-          :name="effect.name"
-          :isSelected="selectedEffect?.id === effect.id"
-          @select="onSelectorButtonClick(effect)" 
-          >
+        <SelectorButton v-for="effect in effectList.slice((page - 1) * 16, page * 16)" :key="effect.id" :id="effect.id"
+          :name="effect.name" :isSelected="selectedEffect?.id === effect.id" @select="onSelectorButtonClick(effect)">
         </SelectorButton>
       </div>
       <div v-else class="selector">
-        <SelectorButtonEditor
-          v-for="effect in effectList.slice((page - 1) * 16, page * 16)" :key="effect.id" 
-          :id="effect.id" 
-          :name="effect.name"
-          @edit="e => onSelectorEdit(e, effect.id)"
-          >
+        <SelectorButtonEditor v-for="effect in effectList.slice((page - 1) * 16, page * 16)" :key="effect.id"
+          :id="effect.id" :name="effect.name" @edit="e => onSelectorEdit(e, effect.id)">
         </SelectorButtonEditor>
       </div>
     </div>
@@ -246,6 +247,10 @@ function previousPage() {
 </template>
 
 <style scoped>
+p {
+  margin: 5px 0px;
+}
+
 .selector {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -253,11 +258,21 @@ function previousPage() {
   margin: 20px 0;
 }
 
-.websocket-status {
+.flex-row {
   display: flex;
   flex-direction: row;
   gap: 10px;
   align-items: center;
+}
+
+.flex-column {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+
+.gap-20 {
+  gap: 6rem;
 }
 
 .app-container {
---
+++ src/components/StopButton.vue
@@ -0,0 +1,70 @@
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+const props = defineProps<{
+  isSelected: boolean;
+}>();
+const emit = defineEmits(['stop']);
+
+const onClick = () => {
+  // Emit the select event with the id
+  emit('stop');
+};
+
+const toggleIndicator = computed(() => {
+  return props.isSelected ? 'toggle-selected' : 'toggle-unselected';
+});
+
+</script>
+
+<template>
+  <button class="selector-button" @click="onClick">
+    <p class="selector-label">
+      STOP     
+    </p>
+    <div :class="toggleIndicator"></div>
+  </button>
+</template>
+
+<style scoped>
+.selector-button {
+  background-color: white; /* Green */
+  border: 2px solid red; /* Green */
+  padding: 3rem 3rem;
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+.selector-label {
+  font-size: 1rem;
+  font-weight: bold;
+  color: black;
+  margin: 0;
+}
+
+.selector-id {
+  font-size: 1rem;
+  color: gray;
+  margin: 0;
+}
+
+.toggle-unselected {
+  width: 4rem;
+  height: 1rem;
+  border: 2px solid black; /* Green */
+  background-color: white; /* Green */
+  position: relative;
+}
+
+.toggle-selected {
+  width: 4rem;
+  height: 1rem;
+  border: 2px solid black; /* Green */
+  background-color: red; /* Green */
+}
+</style>
